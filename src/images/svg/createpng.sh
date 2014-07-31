45b16470948d7061bfe6a6ee9a0555f387c13105
Doug Fritz <dougfritz@google.com>
2014-07-31 16:18:19 -0700

    add more images
    Change-Id: Id6d0e139722b917d3197dfa96a88454e83921c25

--- src/images/svg/createpng.sh
+++ src/images/svg/createpng.sh
@@ -1,6 +1,6 @@
 rm ../*.png
 for name in default recording recording-pulse saving; do
-  for sz in 128 38; do
+  for sz in 16 19 32 38 48 128; do
     convert -background none icon-$name.svg -resize $sz ../icon-$name-$sz.png
   done
 done
